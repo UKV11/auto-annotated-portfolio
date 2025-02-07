7b6f8b781b4309f6b15d45fd0bc6ff557639ce6f
UKV11 <krishna11vamsi112000@gmail.com>
2025-02-07 10:49:46 -0600

    Update CertificationsSection.tsx

--- src/components/sections/CertificationsSection.tsx
+++ src/components/sections/CertificationsSection.tsx
@@ -3,7 +3,7 @@ import section from '../section';
 
 export function CertificationsSection() {
     return (
-        <Section>
+        <section>
             <h2 className="text-3xl font-bold text-center">Certifications</h2>
             <ul className="mt-4 list-disc pl-5">
                 <li><strong>Google Advanced Data Analytics</strong> - Google</li>
